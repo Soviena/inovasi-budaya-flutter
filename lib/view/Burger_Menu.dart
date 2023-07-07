87451d7c1501b81a9e19193621f344ddbd418238
Kemaru345 <100782621+Kemaru345@users.noreply.github.com>
2023-07-07 09:59:28 +0700

    mengatur arrow icon

--- lib/view/Burger_Menu.dart
+++ lib/view/Burger_Menu.dart
@@ -25,7 +25,7 @@ class _BurgerListState extends State<BurgerList> {
         child: Column(
           children: [
             Container(
-              //tombol back
+              // Tombol back
               height: 56,
               alignment: Alignment.topLeft,
               child: IconButton(
@@ -39,49 +39,43 @@ class _BurgerListState extends State<BurgerList> {
                 },
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 5),
-                  child: Row(children: [
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                    GestureDetector(
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Menu", style: TextStyle(color: Colors.orange))
-                          ],
-                        ),
-                      ),
-                    ),
-                  ]),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
-                    );
-                  },
-                  child: Container(
-                    margin: EdgeInsets.only(right: 240),
-                    child: const Icon(
-                      Icons.arrow_back,
-                      color: Colors.orange,
-                      size: 40,
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              child: Row(
+                children: [
+                  Container(
+                    width: 5,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
                     ),
                   ),
-                ),
-              ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                      );
+                    },
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 14),
+                          child: Icon(
+                            Icons.arrow_back,
+                            color: Colors.orange,
+                            size: 40,
+                          ),
+                        ),
+                        Text(
+                          "Menu",
+                          style: TextStyle(color: Colors.orange, fontSize: 32),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
             GestureDetector(
               onTap: () {
@@ -93,7 +87,8 @@ class _BurgerListState extends State<BurgerList> {
               child: Container(
                 margin: EdgeInsets.fromLTRB(0, 40, 190, 0),
                 decoration: const BoxDecoration(
-                    border: Border(bottom: BorderSide(color: Colors.white))),
+                  border: Border(bottom: BorderSide(color: Colors.white)),
+                ),
                 child: const Text(
                   "Home",
                   style: TextStyle(fontSize: 30, color: Colors.white),
@@ -110,7 +105,8 @@ class _BurgerListState extends State<BurgerList> {
               child: Container(
                 margin: EdgeInsets.fromLTRB(0, 40, 70, 0),
                 decoration: const BoxDecoration(
-                    border: Border(bottom: BorderSide(color: Colors.white))),
+                  border: Border(bottom: BorderSide(color: Colors.white)),
+                ),
                 child: const Text(
                   "Jadwal Budaya",
                   style: TextStyle(fontSize: 30, color: Colors.white),
@@ -127,7 +123,8 @@ class _BurgerListState extends State<BurgerList> {
               child: Container(
                 margin: EdgeInsets.fromLTRB(0, 40, 190, 0),
                 decoration: const BoxDecoration(
-                    border: Border(bottom: BorderSide(color: Colors.white))),
+                  border: Border(bottom: BorderSide(color: Colors.white)),
+                ),
                 child: const Text(
                   "Materi",
                   style: TextStyle(fontSize: 30, color: Colors.white),
@@ -144,7 +141,8 @@ class _BurgerListState extends State<BurgerList> {
               child: Container(
                 margin: EdgeInsets.fromLTRB(0, 40, 190, 0),
                 decoration: const BoxDecoration(
-                    border: Border(bottom: BorderSide(color: Colors.white))),
+                  border: Border(bottom: BorderSide(color: Colors.white)),
+                ),
                 child: const Text(
                   "Kinerja",
                   style: TextStyle(fontSize: 30, color: Colors.white),
@@ -161,7 +159,8 @@ class _BurgerListState extends State<BurgerList> {
               child: Container(
                 margin: EdgeInsets.fromLTRB(0, 40, 80, 0),
                 decoration: const BoxDecoration(
-                    border: Border(bottom: BorderSide(color: Colors.white))),
+                  border: Border(bottom: BorderSide(color: Colors.white)),
+                ),
                 child: const Text(
                   "Safety Moment",
                   style: TextStyle(fontSize: 30, color: Colors.white),
@@ -178,7 +177,8 @@ class _BurgerListState extends State<BurgerList> {
               child: Container(
                 margin: EdgeInsets.fromLTRB(0, 40, 200, 0),
                 decoration: const BoxDecoration(
-                    border: Border(bottom: BorderSide(color: Colors.white))),
+                  border: Border(bottom: BorderSide(color: Colors.white)),
+                ),
                 child: const Text(
                   "Profile",
                   style: TextStyle(fontSize: 30, color: Colors.white),
@@ -195,7 +195,8 @@ class _BurgerListState extends State<BurgerList> {
               child: Container(
                 margin: EdgeInsets.fromLTRB(0, 40, 135, 0),
                 decoration: const BoxDecoration(
-                    border: Border(bottom: BorderSide(color: Colors.white))),
+                  border: Border(bottom: BorderSide(color: Colors.white)),
+                ),
                 child: const Text(
                   "Pengaturan",
                   style: TextStyle(fontSize: 30, color: Colors.white),
@@ -212,7 +213,8 @@ class _BurgerListState extends State<BurgerList> {
               child: Container(
                 margin: EdgeInsets.fromLTRB(0, 40, 185, 0),
                 decoration: const BoxDecoration(
-                    border: Border(bottom: BorderSide(color: Colors.white))),
+                  border: Border(bottom: BorderSide(color: Colors.white)),
+                ),
                 child: const Text(
                   "Log Out",
                   style: TextStyle(fontSize: 30, color: Colors.white),
