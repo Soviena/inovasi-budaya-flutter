1ff8b874a875d1973589abe9160f8d7853328792
Fiffah <itsmefiffah@gmail.com>
2023-07-07 14:53:08 +0700

    banyak pokoknya

--- lib/view/SafetyMoment.dart
+++ lib/view/SafetyMoment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:inovasi_budaya/view/Burger_Menu.dart';
 
 class Safety_Moment extends StatelessWidget {
@@ -36,6 +37,99 @@ class Safety_Moment extends StatelessWidget {
               SizedBox(width: 48),
             ],
           ),
+        ),
+        body: ListView(
+          padding: EdgeInsets.all(24.0),
+          children: [
+            Align(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 26, 73, 128),
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            topLeft: Radius.circular(20))),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Judul Flyer',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange,
+                      ),
+                    ),
+                  ),
+                  Image.asset('assets/image/Poster1.png'),
+                  Container(
+                    height: 20,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 26, 73, 128),
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(20))),
+                  ),
+                  SizedBox(height: 35),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 26, 73, 128),
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            topLeft: Radius.circular(20))),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Judul Flyer',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange,
+                      ),
+                    ),
+                  ),
+                  Image.asset('assets/image/Poster2.png'),
+                  Container(
+                    height: 20,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 26, 73, 128),
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(20))),
+                  ),
+                  SizedBox(height: 35),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 26, 73, 128),
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            topLeft: Radius.circular(20))),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Judul Flyer',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange,
+                      ),
+                    ),
+                  ),
+                  Image.asset('assets/image/Poster3.png'),
+                  Container(
+                    height: 20,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 26, 73, 128),
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(20))),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
