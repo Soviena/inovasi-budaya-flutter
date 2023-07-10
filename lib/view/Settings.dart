41b5094dd61a2279c86fcc312af1652147eb5941
Fiffah <itsmefiffah@gmail.com>
2023-07-10 16:44:19 +0700

    update settings

--- lib/view/Settings.dart
+++ lib/view/Settings.dart
@@ -1,44 +1,162 @@
 import 'package:flutter/material.dart';
 import 'package:inovasi_budaya/view/Burger_Menu.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  final items = ['Tiap hari 1x', 'Interval'];
+  String? value;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 6, 51, 83),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromARGB(255, 6, 51, 83),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.orange,
+              ),
+              iconSize: 36,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => BurgerList()),
+                );
+              },
+            ),
+            Text(
+              'Pengaturan',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.orange,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 6, 51, 83),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.fromLTRB(0, 50, 0, 300),
+          color: Color.fromARGB(150, 103, 103, 103),
+          child: Column(
             children: [
-              IconButton(
-                icon: Icon(
-                  Icons.menu,
-                  color: Colors.orange,
-                ),
-                iconSize: 36,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => BurgerList()),
-                  );
-                },
+              Row(
+                children: [
+                  Container(
+                    width: 150,
+                    margin: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                    child: Text(
+                      'Notifikasi Peregangan',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 120,
+                    margin: EdgeInsets.only(top: 10, left: 100),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton<String>(
+                        value: value,
+                        iconSize: 36,
+                        icon: Icon(
+                          Icons.arrow_drop_down,
+                          color: Colors.black,
+                        ),
+                        isExpanded: true,
+                        items: items.map(BuildMenuItem).toList(),
+                        onChanged: (value) =>
+                            setState(() => this.value = value),
+                        menuMaxHeight: 40,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                'Pengaturan',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.orange,
-                ),
+              Row(
+                children: [
+                  Container(
+                    width: 100,
+                    margin: EdgeInsets.fromLTRB(10, 40, 0, 0),
+                    child: Text(
+                      'Pada jam',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    margin: EdgeInsets.only(top: 50, left: 160),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.orange, width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color.fromARGB(20, 0, 0, 0)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(width: 48),
+              Row(
+                children: [
+                  Container(
+                    width: 120,
+                    margin: EdgeInsets.fromLTRB(10, 60, 0, 0),
+                    child: Text(
+                      'Tiap (menit)',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    margin: EdgeInsets.only(top: 50, left: 140),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.orange, width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color.fromARGB(20, 0, 0, 0)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
-        backgroundColor: Color.fromARGB(255, 6, 51, 83),
       ),
     );
   }
+
+  DropdownMenuItem<String> BuildMenuItem(String item) => DropdownMenuItem(
+        value: item,
+        child: Text(
+          item,
+        ),
+      );
 }
