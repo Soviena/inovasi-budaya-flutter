274eba747e1c2bac82568b9538264a829fd923c8
Muhammad Rovino Sanjaya <rovino.rs@gmail.com>
2023-07-12 11:04:54 +0700

    fix notification in background

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,9 @@ import 'package:inovasi_budaya/view/register.dart';
 import 'package:inovasi_budaya/view/splash_view.dart';
 import 'package:inovasi_budaya/view/HomePage.dart';
 
+import 'package:inovasi_budaya/local_notification_helper.dart';
+import 'package:workmanager/workmanager.dart';
+
 String? token;
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
@@ -26,6 +29,30 @@ getToken() async {
   print("Token : " + token.toString());
 }
 
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    // initialise the plugin of flutterlocalnotifications.
+    final FlutterLocalNotificationsPlugin notif =
+        FlutterLocalNotificationsPlugin();
+
+    notif.show(
+        0,
+        "Testing ",
+        "This is an Flutter Push Notification",
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+              "high_importance_channel", "High Importance Notifications",
+              channelDescription:
+                  "This channel is used for important notifications.",
+              importance: Importance.high,
+              color: Colors.blue,
+              playSound: true,
+              icon: '@mipmap/ic_launcher'),
+        ));
+    return Future.value(true);
+  });
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -40,6 +67,25 @@ void main() async {
     sound: true,
   );
   getToken();
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(
+
+      // The top level function, aka callbackDispatcher
+      callbackDispatcher,
+
+      // If enabled it will post a notification whenever
+      // the task is running. Handy for debugging tasks
+      isInDebugMode: true);
+  // Periodic task registration
+  Workmanager().registerPeriodicTask(
+    "2",
+
+    //This is the value that will be
+    // returned in the callbackDispatcher
+    "simplePeriodicTask",
+
+    frequency: Duration(minutes: 15),
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
