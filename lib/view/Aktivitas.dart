0c5c4891c0147ce52825cd5d02bb51bea63f7f89
Fiffah <itsmefiffah@gmail.com>
2023-07-12 14:49:03 +0700

    add accordion

--- lib/view/Aktivitas.dart
+++ lib/view/Aktivitas.dart
@@ -1,7 +1,16 @@
+import 'package:accordion/accordion.dart';
 import 'package:flutter/material.dart';
 import 'package:inovasi_budaya/view/Burger_Menu.dart';
 
 class Budaya extends StatelessWidget {
+  final _headerStyle = const TextStyle(
+      color: Color(0xffffffff), fontSize: 15, fontWeight: FontWeight.bold);
+  final _contentStyleHeader = const TextStyle(
+      color: Color(0xff999999), fontSize: 14, fontWeight: FontWeight.w700);
+  final _contentStyle = const TextStyle(
+      color: Color(0xff999999), fontSize: 14, fontWeight: FontWeight.normal);
+  final _loremIpsum =
+      '''Lorem ipsum is typically a corrupted version of 'De finibus bonorum et malorum', a 1st century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin.''';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,6 +45,27 @@ class Budaya extends StatelessWidget {
           ],
         ),
       ),
+      body: Accordion(
+          maxOpenSections: 2,
+          headerBackgroundColorOpened: Colors.black54,
+          scaleWhenAnimating: true,
+          openAndCloseAnimation: true,
+          headerPadding:
+              const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+          children: [
+            AccordionSection(
+              isOpen: true,
+              leftIcon: const Icon(Icons.insights_rounded, color: Colors.white),
+              headerBackgroundColor: Colors.black,
+              headerBackgroundColorOpened: Colors.red,
+              header: Text('Introduction', style: _headerStyle),
+              content: Text(_loremIpsum, style: _contentStyle),
+              contentHorizontalPadding: 20,
+              contentBorderWidth: 1,
+              // onOpenSection: () => print('onOpenSection ...'),
+              // onCloseSection: () => print('onCloseSection ...'),
+            ),
+          ]),
     );
   }
 }
