53c080cff3e5e54cc11058790562194a2428c93a
Fiffah <itsmefiffah@gmail.com>
2023-07-13 08:28:47 +0700

    nambah accordion

--- lib/view/Aktivitas.dart
+++ lib/view/Aktivitas.dart
@@ -2,15 +2,23 @@ import 'package:accordion/accordion.dart';
 import 'package:flutter/material.dart';
 import 'package:inovasi_budaya/view/Burger_Menu.dart';
 
-class Budaya extends StatelessWidget {
+class Budaya extends StatefulWidget {
+  @override
+  State<Budaya> createState() => _BudayaState();
+}
+
+class _BudayaState extends State<Budaya> {
   final _headerStyle = const TextStyle(
-      color: Color(0xffffffff), fontSize: 15, fontWeight: FontWeight.bold);
-  final _contentStyleHeader = const TextStyle(
-      color: Color(0xff999999), fontSize: 14, fontWeight: FontWeight.w700);
+      color: Color.fromARGB(255, 136, 136, 136),
+      fontSize: 24,
+      fontWeight: FontWeight.bold);
+
   final _contentStyle = const TextStyle(
-      color: Color(0xff999999), fontSize: 14, fontWeight: FontWeight.normal);
+      color: Colors.black, fontSize: 14, fontWeight: FontWeight.w900);
+
   final _loremIpsum =
       '''Lorem ipsum is typically a corrupted version of 'De finibus bonorum et malorum', a 1st century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin.''';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,24 +54,374 @@ class Budaya extends StatelessWidget {
         ),
       ),
       body: Accordion(
-          maxOpenSections: 2,
           headerBackgroundColorOpened: Colors.black54,
           scaleWhenAnimating: true,
           openAndCloseAnimation: true,
           headerPadding:
-              const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+              const EdgeInsets.symmetric(vertical: 13, horizontal: 15),
           children: [
             AccordionSection(
-              isOpen: true,
-              leftIcon: const Icon(Icons.insights_rounded, color: Colors.white),
-              headerBackgroundColor: Colors.black,
-              headerBackgroundColorOpened: Colors.red,
-              header: Text('Introduction', style: _headerStyle),
-              content: Text(_loremIpsum, style: _contentStyle),
-              contentHorizontalPadding: 20,
+              isOpen: false,
+
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('Januari', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
               contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
               // onOpenSection: () => print('onOpenSection ...'),
               // onCloseSection: () => print('onCloseSection ...'),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('Februari', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('Maret', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('April', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('Mei', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('Juni', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('Juli', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('Agustus', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('September', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('Oktober', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('November', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
+            ),
+            AccordionSection(
+              isOpen: false,
+              headerBackgroundColor: Color.fromARGB(100, 136, 136, 136),
+              headerBackgroundColorOpened: Color.fromARGB(255, 250, 131, 15),
+              header: Text('Desember', style: _headerStyle),
+              content: Column(
+                children: [
+                  Text(_loremIpsum, style: _contentStyle),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromRGBO(252, 178, 106, 1))),
+                    ),
+                  ),
+                ],
+              ),
+              contentHorizontalPadding: 10,
+              contentBorderWidth: 1,
+              contentBackgroundColor: Color.fromRGBO(252, 178, 106, 0.21),
+              contentBorderColor: Color.fromRGBO(252, 178, 106, 0.21),
             ),
           ]),
     );
