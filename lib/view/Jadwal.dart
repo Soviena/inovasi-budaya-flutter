4a1827260de5711f3ae65a72d101966b01c4dbd6
Muhammad Rovino Sanjaya <rovino.rs@gmail.com>
2023-07-20 16:46:24 +0700

    optimisasi bug fix, backend budaya

--- lib/view/Jadwal.dart
+++ lib/view/Jadwal.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:inovasi_budaya/view/burger_menu.dart';
 import 'package:accordion/accordion.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+// import 'package:quickalert/quickalert.dart';
 
 class Jadwal extends StatefulWidget {
   const Jadwal({super.key});
@@ -18,8 +23,85 @@ class _JadwalState extends State<Jadwal> {
   final _contentStyle = const TextStyle(
       color: Colors.black, fontSize: 14, fontWeight: FontWeight.w900);
 
-  final _loremIpsum =
-      '''Lorem ipsum is typically a corrupted version of 'De finibus bonorum et malorum', a 1st century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin.''';
+  List<AccordionSection> list = [];
+
+  Future<dynamic> getData() async {
+    var response =
+        await http.get(Uri.parse("http://192.168.1.124:8000/api/budaya/all"));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      return null;
+    }
+  }
+
+  void makeAccordion() async {
+    dynamic data = await getData();
+    for (var budaya in data) {
+      list.add(AccordionSection(
+        headerBackgroundColor: const Color.fromARGB(100, 136, 136, 136),
+        headerBackgroundColorOpened: const Color.fromARGB(255, 250, 131, 15),
+        header: Text(
+            DateFormat.yMMMM()
+                .format(DateTime.parse(budaya['tanggal'] + "-01")),
+            style: _headerStyle),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 20),
+              alignment: Alignment.topLeft,
+              child: Text(
+                budaya['judul'],
+                style: _contentStyle,
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                budaya['deskripsi'],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              margin: const EdgeInsets.only(top: 10),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/aktivitas");
+                },
+                style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                        Color.fromRGBO(252, 178, 106, 1))),
+                child: const Text(
+                  'Aktivitas',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
+        contentHorizontalPadding: 10,
+        contentBorderWidth: 1,
+        contentBackgroundColor: const Color.fromRGBO(252, 178, 106, 0.21),
+        contentBorderColor: const Color.fromRGBO(252, 178, 106, 0.21),
+        // onOpenSection: () => print('onOpenSection ...'),
+        // onCloseSection: () => print('onCloseSection ...'),
+      ));
+    }
+    setState(() {
+      list;
+    });
+    return;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    makeAccordion();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,42 +134,7 @@ class _JadwalState extends State<Jadwal> {
           scaleWhenAnimating: false,
           headerPadding:
               const EdgeInsets.symmetric(vertical: 13, horizontal: 15),
-          children: [
-            AccordionSection(
-              headerBackgroundColor: const Color.fromARGB(100, 136, 136, 136),
-              headerBackgroundColorOpened:
-                  const Color.fromARGB(255, 250, 131, 15),
-              header: Text('Januari', style: _headerStyle),
-              content: Column(
-                children: [
-                  Text(_loremIpsum, style: _contentStyle),
-                  Container(
-                    alignment: Alignment.centerRight,
-                    margin: const EdgeInsets.only(top: 10),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, "/aktivitas");
-                      },
-                      style: const ButtonStyle(
-                          backgroundColor: MaterialStatePropertyAll(
-                              Color.fromRGBO(252, 178, 106, 1))),
-                      child: const Text(
-                        'Aktivitas',
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              contentHorizontalPadding: 10,
-              contentBorderWidth: 1,
-              contentBackgroundColor: const Color.fromRGBO(252, 178, 106, 0.21),
-              contentBorderColor: const Color.fromRGBO(252, 178, 106, 0.21),
-              // onOpenSection: () => print('onOpenSection ...'),
-              // onCloseSection: () => print('onCloseSection ...'),
-            ),
-          ]),
+          children: list),
     );
   }
 }
