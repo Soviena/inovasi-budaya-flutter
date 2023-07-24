33735ab6fca6bd20614f0fa265d6a533a287ee1e
Muhammad Rovino Sanjaya <rovino.rs@gmail.com>
2023-07-24 16:29:58 +0700

    Menambahkan backend pada profil, image picker crop

--- lib/view/Profil.dart
+++ lib/view/Profil.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:inovasi_budaya/dbHelper.dart';
 import 'package:inovasi_budaya/view/burger_menu.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
+
+import 'dart:io';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -9,13 +14,60 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  String url = "http://192.168.1.124:8000/";
   final TextEditingController name = TextEditingController();
   final TextEditingController email = TextEditingController();
-  final TextEditingController tanggalLahir = TextEditingController();
-
+  final picker = ImagePicker();
+  final cropper = ImageCropper();
+  late String tanggalLahir;
   bool editState = false;
 
   dynamic user = {};
+
+  void getData() async {
+    user = await DatabaseHelper.instance.getSession();
+    setState(() {
+      name.text = user['name'];
+      email.text = user['email'];
+      tanggalLahir = user['dob'];
+      user;
+    });
+  }
+
+  Future<CroppedFile?> _cropImage(File imageFile) async {
+    CroppedFile? croppedFile = await cropper.cropImage(
+      sourcePath: imageFile.path,
+      aspectRatio: const CropAspectRatio(
+          ratioX: 1, ratioY: 1), // You can adjust the aspect ratio as needed.
+      compressQuality: 100, // Set the image quality after cropping (0 to 100).
+      maxWidth: 800, // Set the maximum width of the cropped image.
+      maxHeight: 800, // Set the maximum height of the cropped image.
+      uiSettings: [
+        AndroidUiSettings(
+          toolbarTitle: 'Crop Image',
+          toolbarColor: Colors.deepOrange,
+          toolbarWidgetColor: Colors.white,
+          initAspectRatio: CropAspectRatioPreset.square,
+          lockAspectRatio: true,
+        ),
+        IOSUiSettings(
+          minimumAspectRatio: 1.0,
+        ),
+      ],
+    );
+
+    return croppedFile;
+  }
+
+  Future<File?> _pickImage() async {
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      await _cropImage(File(pickedImage.path));
+      return null;
+    } else {
+      return null;
+    }
+  }
 
   Widget profileContainer() {
     if (editState) {
@@ -24,34 +76,28 @@ class _ProfileState extends State<Profile> {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                width: 120,
-                height: 120,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.grey,
-                ),
-                child: Stack(children: [
-                  Image.asset(
-                    "assets/image/Inspeksi.png",
-                    fit: BoxFit.cover,
-                  ),
-                  Container(
-                    height: 120,
-                    width: 120,
-                    decoration: const BoxDecoration(
+            GestureDetector(
+              onTap: () {
+                _pickImage();
+              },
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       color: Colors.black38,
-                    ),
-                    child: const Icon(
-                      Icons.edit,
-                      color: Colors.white,
-                      size: 50,
-                    ),
-                  )
-                ]),
+                      image: DecorationImage(
+                          opacity: 100,
+                          image: NetworkImage(
+                              "${url}storage/uploaded/user/${user['profilePic']}"))),
+                  child: const Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                    size: 50,
+                  ),
+                ),
               ),
             ),
             Column(
@@ -126,16 +172,35 @@ class _ProfileState extends State<Profile> {
                         ),
                       ),
                     ),
-                    Container(
-                      height: 30,
-                      width: double.infinity,
-                      padding: const EdgeInsets.symmetric(horizontal: 5),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        border: Border.all(color: Colors.orange, width: 1),
-                        color: Colors.white,
+                    GestureDetector(
+                      onTap: () {
+                        showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.parse("1950-01-01"),
+                          lastDate: DateTime.now(),
+                        ).then(
+                          (date) => {
+                            setState(() {
+                              tanggalLahir =
+                                  "${date?.year}-${date?.month}-${date?.day}";
+                            })
+                          },
+                        );
+                      },
+                      child: Container(
+                        height: 30,
+                        alignment: Alignment.bottomLeft,
+                        width: double.infinity,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 5, vertical: 2),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          border: Border.all(color: Colors.orange, width: 1),
+                          color: Colors.white,
+                        ),
+                        child: Text(tanggalLahir),
                       ),
-                      child: TextField(controller: tanggalLahir),
                     ),
                   ],
                 ),
@@ -186,14 +251,12 @@ class _ProfileState extends State<Profile> {
               child: Container(
                 width: 120,
                 height: 120,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.grey,
-                ),
-                child: Image.asset(
-                  "assets/image/Inspeksi.png",
-                  fit: BoxFit.cover,
-                ),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "${url}storage/uploaded/user/${user['profilePic']}"))),
               ),
             ),
             Column(
@@ -205,7 +268,7 @@ class _ProfileState extends State<Profile> {
                     Container(
                       margin: const EdgeInsets.symmetric(vertical: 10),
                       child: const Text(
-                        'Nama',
+                        "Nama",
                         style: TextStyle(
                           color: Colors.white70,
                           fontWeight: FontWeight.w500,
@@ -219,6 +282,15 @@ class _ProfileState extends State<Profile> {
                         borderRadius: BorderRadius.circular(5),
                         border: Border.all(color: Colors.orange, width: 1),
                         color: Colors.white70,
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          user['name'],
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 6, 51, 83)),
+                        ),
                       ),
                     ),
                   ],
@@ -244,6 +316,15 @@ class _ProfileState extends State<Profile> {
                         border: Border.all(color: Colors.orange, width: 1),
                         color: Colors.white70,
                       ),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          user['email'],
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 6, 51, 83)),
+                        ),
+                      ),
                     ),
                   ],
                 ),
@@ -267,6 +348,15 @@ class _ProfileState extends State<Profile> {
                         borderRadius: BorderRadius.circular(5),
                         border: Border.all(color: Colors.orange, width: 1),
                         color: Colors.white70,
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          user['dob'],
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 6, 51, 83)),
+                        ),
                       ),
                     ),
                   ],
@@ -295,7 +385,7 @@ class _ProfileState extends State<Profile> {
                   ),
                 ),
                 child: const Text(
-                  'edit',
+                  'Edit',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
@@ -308,6 +398,13 @@ class _ProfileState extends State<Profile> {
         ),
       );
     }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getData();
+    super.initState();
   }
 
   @override
